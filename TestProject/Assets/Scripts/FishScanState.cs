c605b95dfb28960c7e4df6df11ed2501ba043293
IanKervinSullano <iankervin.sullano@benilde.edu.ph>
2026-06-20 03:54:11 +0800

    Spawner + Scan Minigame Combinatiom
    spawner and scan minigame connected

--- TestProject/Assets/Scripts/FishScanState.cs
+++ TestProject/Assets/Scripts/FishScanState.cs
@@ -2,5 +2,49 @@ using UnityEngine;
 
 public class TestFish : MonoBehaviour
 {
+    public enum FishType
+    {
+        SmallFish,
+        MediumFish,
+        LargeFish,
+        HostileFish
+    }
+
+    [Header("Fish Data")]
+    public FishType fishType;
     public bool alreadyScanned = false;
+
+    private SpriteRenderer sr;
+    private Color originalColor;
+
+    [Header("Hover Highlight")]
+    [Range(0f, 1f)]
+    public float highlightAlpha = 0.25f;
+
+    public Color highlightTint = new Color(0f, 1f, 1f, 1f);
+
+    void Awake()
+    {
+        sr = GetComponentInChildren<SpriteRenderer>();
+
+        if (sr != null)
+            originalColor = sr.color;
+    }
+
+    public void SetHighlighted(bool state)
+    {
+        if (sr == null) return;
+
+        if (state)
+        {
+            // subtle cyan overlay
+            Color c = highlightTint;
+            c.a = highlightAlpha;
+            sr.color = c;
+        }
+        else
+        {
+            sr.color = originalColor;
+        }
+    }
 }
