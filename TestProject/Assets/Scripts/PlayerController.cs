309844c6bfc2b3539a2fdb8c426ec8b042676c8c
IanKervinSullano <iankervin.sullano@benilde.edu.ph>
2026-06-19 21:02:35 +0800

    temp
    Unfinished Scan Minigame

--- TestProject/Assets/Scripts/PlayerController.cs
+++ TestProject/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     private float moveForce;
     private float maxSpeed;
 
+    private ColorMemoryGame colorMemoryGame;
+
     private Vector2 moveInput;
 
     [SerializeField]private Rigidbody2D rb; 
@@ -15,12 +17,15 @@ public class PlayerController : MonoBehaviour
     }
     private void Start()
     {
+        colorMemoryGame = GetComponent<ColorMemoryGame>();
         moveForce = 5f;
         maxSpeed = 25; 
     }
     private void Update()
     {
-        GetMoveInput(); 
+        GetMoveInput();
+        MouseTracker();
+        ScanFish();
     }
     private void FixedUpdate()
     {
@@ -36,4 +41,22 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));  
     }
+
+    private void MouseTracker()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        Vector3 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    private void ScanFish()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            colorMemoryGame.StartScanMinigame();
+        }
+    }
+
 }
